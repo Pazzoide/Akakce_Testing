2390a2ec5d70c559136edf4bcf158a5ed01bdef2
BURHAN <Brhn.Sngl.95@outlook.com>
2023-08-18 19:48:22 +0300

    Başlangıç

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,20 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseDriver {
+    public static WebDriver driver;
+    static{
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    public static void BekleVeKapat(){
+        MyFunc.Wait(5);
+        driver.quit();
+    }
+}
