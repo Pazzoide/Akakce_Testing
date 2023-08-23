080848e94c4f00c90e121df17cb232fb71b932ed
zafer cansiz <zfrcnsz1@gmail.com>
2023-08-23 21:36:32 +0400

    Oturum Acma Login Negatif Testler Eklendi

--- src/OturumAcmaLogin.java
+++ src/OturumAcmaLogin.java
@@ -1,5 +1,6 @@
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -28,6 +29,53 @@ public class OturumAcmaLogin extends BaseDriver {
 
         BekleVeKapat();
 
+
+    }
+    @Test
+    public void TestNegative1(){
+        driver.get("https://www.akakce.com/");
+
+        WebElement loginNavigator = driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        loginNavigator.click();
+        MyFunc.Wait(1);
+
+        WebElement eMailField = driver.findElement(By.xpath("//input[@id='life']"));
+        eMailField.sendKeys("zaferCansiz1@gmail.com");
+        MyFunc.Wait(1);
+
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='lifp']"));
+        passwordField.sendKeys("HataliSifre1");
+        MyFunc.Wait(1);
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='lfb']"));
+        loginButton.click();
+        MyFunc.Wait(1);
+
+        WebElement message = driver.findElement(By.xpath("//div[@class='alertX t2']"));
+
+        Assert.assertTrue("Uyari mesaji gorulmedi",message.getText().contains("Şifre doğru değil."));
+
+        BekleVeKapat();
+
+    }
+
+    @Test
+    public void TestNegative2(){
+        driver.get("https://www.akakce.com/");
+
+        WebElement loginNavigator = driver.findElement(By.xpath("//a[text()='Giriş Yap']"));
+        loginNavigator.click();
+        MyFunc.Wait(1);
+
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='lfb']"));
+        loginButton.click();
+        MyFunc.Wait(1);
+
+        WebElement message = driver.findElement(By.xpath("//div[@class='alertX t2']"));
+
+        Assert.assertTrue("Uyari mesaji gorulmedi",message.getText().contains("Lütfen e-posta adresinizi yazın."));
+
+        BekleVeKapat();
 
     }
 }
